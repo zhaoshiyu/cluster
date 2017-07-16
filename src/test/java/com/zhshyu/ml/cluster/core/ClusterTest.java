767a74484bccbcd1fabd332f14d4fb07797a44cd
Zhao Shiyu <shiyuzhaocn@outlook.com>
2017-07-16 20:53:48 +0800

    add cluster test

--- src/test/java/com/zhshyu/ml/cluster/core/ClusterTest.java
+++ src/test/java/com/zhshyu/ml/cluster/core/ClusterTest.java
@@ -3,8 +3,6 @@ package com.zhshyu.ml.cluster.core;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.panayotis.gnuplot.JavaPlot;
-
 import junit.framework.TestCase;
 
 public class ClusterTest extends TestCase {
